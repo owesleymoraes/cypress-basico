e0e485c342ae8c7d8c6663cc97a4dbbd56befa55
Wesley Moraes <wesleyjava88@gmail.com>
2022-10-29 22:58:03 -0300

    feat: refatoração códigos repetidos mais teste com refatoração

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,4 +1,5 @@
 ///<reference types="Cypress"/>
+import { faker } from '@faker-js/faker'
 
 describe("Central de atendimento ao cliente TAT", () => {
     beforeEach(() => {
@@ -6,18 +7,27 @@ describe("Central de atendimento ao cliente TAT", () => {
 
     })
 
+    let nameFaker = faker.internet.userName()
+    let lastNameFaker = faker.internet.userName()
+    let emailFaker = faker.internet.email()
+    let phoneFaker = faker.phone.number()
+    let textFaker = faker.lorem.paragraph()
+
+    beforeEach(() => {
+        cy.get('#firstName').type(nameFaker)
+        cy.get('#lastName').type(lastNameFaker)
+        cy.get('#email').type(emailFaker)
+        cy.get('#phone').type(phoneFaker)
+        cy.get('#open-text-area').type(textFaker, { delay: 0 })
+        cy.get('button[type="submit"]').click()
+    })
+
     it('Verifica o título do aplicativo', () => {
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
     })
 
     it("Preenche os campos obrigatórios e envia o formulário", () => {
-        const longText = 'text, text text text texttext texttext texttext texttext texttext texttext texttext texttext texttext texttext texttext texttext texttext texttext texttext text'
-        cy.get('#firstName').type('Wesley')
-        cy.get('#lastName').type('Moraes')
-        cy.get('#email').type('wesleyjava88@gmail.com')
-        cy.get('#phone').type('3243-3488')
-        cy.get('#open-text-area').type(longText, { delay: 0 })
-        cy.get('button[type="submit"]').click()
+
 
         cy.get('.success').should('be.visible')
     })
@@ -38,7 +48,7 @@ describe("Central de atendimento ao cliente TAT", () => {
         cy.get('#phone').should('have.text', '')
     })
 
-    it('Deve mostrar mensagem de erro quando o telefone se torna obrigratório mas não foi preenchido antes do envio do formulário', () => {
+    it.only('Deve mostrar mensagem de erro quando o telefone se torna obrigratório mas não foi preenchido antes do envio do formulário', () => {
         cy.get('#firstName').type('Wesley')
         cy.get('#lastName').type('Moraes')
         cy.get('#email').type('wesleyjava88@gmail,com')
@@ -60,5 +70,8 @@ describe("Central de atendimento ao cliente TAT", () => {
         cy.get('.error').should('be.visible')
     })
 
+    it('Deve enviar o formulário com sucesso usando um comando customizado', () => {
 
+        cy.get('.success').should('be.visible')
+    })
 })
